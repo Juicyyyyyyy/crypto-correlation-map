5bd2f9ca4df7d609ecb5651d089bda3efa943bc8
Juicyyyyyyy <declan.lowee@gmail.com>
2023-11-05 23:34:10 +0100

    master update

--- app.py
+++ app.py
@@ -2,21 +2,16 @@ from src.data_collection.fetch_data import fetch_historical_data
 from src.data_processing.calculate_returns import calculate_returns, calculate_correlations
 from src.visualization.create_heatmap import create_heatmap
 
-# Define the cryptocurrencies you're interested in and the time period
-symbols = ['BTC/USDT', 'ETH/USDT', 'ADA/USDT', 'XRP/USDT', 'DOT/USDT', 'UNI/USDT', 'LTC/USDT', 'LINK/USDT', 'BCH/USDT', 'XLM/USDT', 'USDC/USDT', 'DOGE/USDT', 'AAVE/USDT', 'ATOM/USDT', 'AVAX/USDT', 'BNB/USDT', 'EOS/USDT', 'ETC/USDT', 'FIL/USDT', 'MKR/USDT', 'NEO/USDT', 'SNX/USDT', 'SOL/USDT', 'TRX/USDT', 'VET/USDT', 'XEM/USDT', 'YFI/USDT', 'ZEC/USDT']
-  # Add more symbols as per your interest
-timeframe = '1d'
-since = 1672444800000  # Start time (1st Jan 2023)
-till = 1677705599000  # End time (30th Jun 2023)
 
-# Fetch historical data, calculate returns, and merge the return data
-# Fetch historical data, calculate returns, and add the return series to the 'returns' dictionary
-returns = {}
-for symbol in symbols:
-    ohlcv = fetch_historical_data(symbol, timeframe, since, till)
-    ohlcv_with_returns = calculate_returns(ohlcv)
-    returns[symbol] = ohlcv_with_returns['return']  # we only want to keep the 'return' series
+def calculate_returns_for_symbols(symbols, timeframe, since, till):
+    returns = {}
+    for symbol in symbols:
+        ohlcv = fetch_historical_data(symbol, timeframe, since, till)
+        ohlcv_with_returns = calculate_returns(ohlcv)
+        returns[symbol] = ohlcv_with_returns['return']
+    return returns
 
-# Calculate correlations and create heatmap
+returns = calculate_returns_for_symbols(['BTC/USDT', 'ETH/USDT', 'ADA/USDT', 'XRP/USDT', 'DOT/USDT'], '1d', 1672444800000, 1677705599000)
+
 correlations = calculate_correlations(returns)
 create_heatmap(correlations)
